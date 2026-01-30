5eb5f757ed60e72409c8a3f619d10dd7439c8e40
qndan <80284796+qndan@users.noreply.github.com>
2026-01-29 17:26:27 -0800

    remove sharing features

--- src/app/__tests__/App.test.tsx
+++ src/app/__tests__/App.test.tsx
@@ -1,5 +1,5 @@
-import { test, expect, afterEach, describe, it } from "vitest";
-import { screen, waitFor } from "@testing-library/react";
+import { test, expect, describe, it } from "vitest";
+import { screen } from "@testing-library/react";
 import { renderFlush, renderWithinWorkspace } from "@/testing-utils/render";
 import userEvent from "@testing-library/user-event";
 
@@ -20,12 +20,6 @@ test.skip("results panel should only be visible after simulating", async () => {
 
   expect(screen.getByTestId(RESULTS_PANEL_TEST_ID)).toBeInTheDocument();
 });
-
-const SAMPLE_SHARE_FRAGMENT =
-  "#s%3DNZHLTsMwEEV%2F5cobpCo0aRGbVkVqKxZIvCRYZuMmE2oSeyp7DFSo%2F44mLSuP7z0zmsev%2BaKYHAezqAoTrCezMG9io1DEE7c0mMI03Kpclnhk28LCq4EuskfrUl9AjgeCC%2F9WUYeyBEccXNODA4E7yJ5AP9YfBjpTaawwourdn70ETyHXYdSfWWiBsoRLyIlaCMOF1jVWCBYNe09BLjBNP6YarnF9h80S%2FWyyrsNGf9sl%2BvlkU4d%2BhhWq6c2tCmM4V2SFaomtPpq%2Bwqy61HzocOR8FQmBBSlHwvfeivbRcoHPnGTkmkEn1TGS83nQ9nZZhIOSKg%2FUSR1MYS7%2BuPJfo4szCyPO05ZzTGQKc7DRehKKSYmkx3h3epiqMBTaczzXc2W%2Fo%2FjSvbILklSrTqfTHw%3D%3D";
-
-const UNREASONABLE_SAMPLE_SHARE_FRAGMENT =
-  "#s%3DNZHLTsMwEEV%2F5cobpCo0aRGbVkVqKxZIvCRYZuMmE2oSeyp7DFSo%2F44mLSuP7z0zmsev%2BaKYHAezqAoTrCezMG9io1DEE7c0mMI03Kpclnhk28LCq4EuskfrUl9AjgeCC%2F9WUYeyBEccXNODA4E7yJ5AP9YfBjpTaawwourdn70ETyHXYdSfWWiBsoRLyIlaCMOF1jVWCBYNe09BLjBNP6YarnF9h80S%2FWyyrsNGf9sl%2BvlkU4d%2BhhWq6c2tCmM4V2SFaomtPpq%2Bwqy61HzocOR8FQmBBSlHwvfeivbRcoHPnGTkmkEn1TGS83nQ9nZZhIOSKg%2FUSR1MYS7%2BuPJfo4szCyPO05ZzTGQKc7DRehKKSYmkx3h3epiqMBTaczzXc2W%2Fo%2FjSvbILklSrqup0Ov0B";
 
 describe.skip("sidebar", () => {
   it("should show steady state if it is available", async () => {
@@ -63,52 +57,5 @@ describe.skip("sidebar", () => {
     await userEvent.click(switchButton);
 
     expect(screen.queryByLabelText("Steady State")).not.toBeInTheDocument();
-  });
-});
-
-describe.skip("sharing", () => {
-  afterEach(() => {
-    location.hash = "";
-  });
-
-  it("should start a simulation when using a share link", async () => {
-    location.hash = SAMPLE_SHARE_FRAGMENT;
-    await renderFlush(<App />);
-
-    await waitFor(() => {
-      expect(screen.getByTestId(RESULTS_PANEL_TEST_ID)).toBeInTheDocument();
-    });
-  });
-
-  it("should not start a simulation when using a share link with unreasonable parameters", async () => {
-    location.hash = UNREASONABLE_SAMPLE_SHARE_FRAGMENT;
-    await renderFlush(<App />);
-
-    await expect(
-      waitFor(
-        () => {
-          expect(screen.getByTestId(RESULTS_PANEL_TEST_ID)).toBeInTheDocument();
-        },
-        { timeout: 250 },
-      ),
-    ).rejects.toThrow();
-
-    // should still set sim parameters
-    expect(
-      +screen.getByLabelText<HTMLInputElement>("Number of Points").value,
-    ).toBeGreaterThan(5000);
-  });
-
-  it("should not start a simulation when not using a share link", async () => {
-    await renderFlush(<App />);
-
-    await expect(
-      waitFor(
-        () => {
-          expect(screen.getByTestId(RESULTS_PANEL_TEST_ID)).toBeInTheDocument();
-        },
-        { timeout: 250 },
-      ),
-    ).rejects.toThrow();
   });
 });
